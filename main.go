472be70c124f10196488364224405089df061e47
Your Name <you@example.com>
2024-11-07 08:00:39 -0500

    removing non-json complaint print statements

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	if !ok {
 		panic("no wifi cards on the system")
 	}
-	fmt.Printf("Using interface: %s\n", iface)
+	// fmt.Printf("Using interface: %s\n", iface)
 
 	wc, err := wireless.NewClient(iface)
 	if err != nil {
